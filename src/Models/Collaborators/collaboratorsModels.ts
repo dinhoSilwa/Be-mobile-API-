700549a8746bb840e269a02dfa27d6c6a2d440bc
claudio silva <claudiosilva.one@gmail.com>
2024-09-16 21:27:56 -0300

    feat : adds collaborator schemas

---
+++ src/Models/Collaborators/collaboratorsModels.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from "mongoose";
+import type { IColalaborator } from "../../types/collaborators/collaboratorsTypes";
+
+export const CollaboratorSchema = new Schema({
+  avatar: { type: String, required: true },
+  name: { type: String, required: true },
+  position: { type: String, required: true },
+  admission: { type: String, required: true },
+  phone: { type: String, required: true },
+});
+
+
+export const CollaboratorModel = model<IColalaborator>("Collaborator", CollaboratorSchema)
