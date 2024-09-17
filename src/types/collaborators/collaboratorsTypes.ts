5ff6f59ad9aa0b455c7cf7e430b7539182ed4d21
claudio silva <claudiosilva.one@gmail.com>
2024-09-16 21:27:16 -0300

    feat : adds collaborators Type

---
+++ src/types/collaborators/collaboratorsTypes.ts
@@ -0,0 +1,8 @@
+import { Document } from "mongoose";
+export interface IColalaborator extends Document {
+  avatar?: string;
+  name: string;
+  position: string;
+  admission: string;
+  phone: string;
+}
