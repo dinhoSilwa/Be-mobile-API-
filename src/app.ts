586058ff4ff7a328ff46e666b6bff60955d1c0be
claudio silva <claudiosilva.one@gmail.com>
2024-09-25 13:57:17 -0300

    teste : allow all domain origins

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,14 @@ import { router } from "./Routes/Collaborator/collaborator.router";
 
 const app: Application = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"], // métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // cabeçalhos permitidos
+  })
+);
+
 app.use(express.json());
 app.use("/api/collaborators", router);
 export default app;
