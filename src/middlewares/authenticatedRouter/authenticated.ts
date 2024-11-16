9425d6d1eb6147fe7741ed0e1bf42125dc44a884
claudio silva <claudiosilva.one@gmail.com>
2024-11-16 13:19:56 -0300

    feat :  add middlewares and token managers

---
+++ src/middlewares/authenticatedRouter/authenticated.ts
@@ -0,0 +1,31 @@
+import type { NextFunction, Request, Response } from "express";
+import { TokenManager } from "../jwtToken/tokenGenerate";
+
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void | Response => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "Token Não Fornecido ou inválido" });
+    }
+    const token = authHeader.split(" ")[1];
+    const tokenManager = TokenManager.getInstance();
+    const decodedToken = tokenManager.verifyToken(token);
+    if (!decodedToken) {
+      return res.status(403).json({ error: "Token inválido ou expirado" });
+    }
+
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? `Falha na autenticação , ${error.message}`
+        : "Falha na autenticação, Erro não indentificado";
+
+    return res.status(500).json({ error: errorMessage });
+  }
+};
