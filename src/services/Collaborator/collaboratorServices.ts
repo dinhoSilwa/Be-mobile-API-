5dfc006e1d8abd03751881fae182cc9217a23370
claudio silva <claudiosilva.one@gmail.com>
2024-11-18 16:49:45 -0300

    refactor: refactor CRUD methods

--- src/services/Collaborator/collaboratorServices.ts
+++ src/services/Collaborator/collaboratorServices.ts
@@ -1,37 +1,38 @@
 import { CollaboratorModel } from "../../Models/Collaborators/collaboratorsModels";
-import type { IColalaborator } from "../../types/collaborators/collaboratorsTypes";
+import type { CollaboratorProps } from "../../types/collaborators/collaboratorsTypes";
 
 export class CollaboratorServices {
   static async createCollaborator(
-    Collaborator: IColalaborator
-  ): Promise<IColalaborator> {
-    const { role_id } = Collaborator;
-
-    const collaborator = new CollaboratorModel({
-      ...Collaborator,
-      role_id,
-    });
+    Collaborator: CollaboratorProps
+  ): Promise<CollaboratorProps> {
+    const collaborator = new CollaboratorModel(Collaborator);
     return await collaborator.save();
   }
 
-  static async getColaborator(id: string): Promise<IColalaborator[] | null> {
+  static async getColaborator(id: string): Promise<CollaboratorProps[] | null> {
     return await CollaboratorModel.findById(id);
   }
 
   static async getAllColaboratorList(
     role_id: string
-  ): Promise<IColalaborator[]> {
-    return await CollaboratorModel.find({ role_id });
+  ): Promise<CollaboratorProps[] | any> {
+    try {
+      return await CollaboratorModel.find({ role_id });
+    } catch (error) {
+      return { msg: "Nenhum dado" };
+    }
   }
 
   static async UpdateCollaborator(
     id: string,
-    data: Partial<IColalaborator>
-  ): Promise<IColalaborator | null> {
+    data: Partial<CollaboratorProps>
+  ): Promise<CollaboratorProps | null> {
     return await CollaboratorModel.findByIdAndUpdate(id, data, { new: true });
   }
 
-  static async deleteCollaborator(id: string): Promise<IColalaborator | null> {
+  static async deleteCollaborator(
+    id: string
+  ): Promise<CollaboratorProps | null> {
     return await CollaboratorModel.findByIdAndDelete(id);
   }
 }
