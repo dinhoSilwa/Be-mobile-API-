9346ece1d3c18394f2d5131a57ee82c136860e49
claudio silva <claudiosilva.one@gmail.com>
2024-11-18 16:50:15 -0300

    refactor: refactor endpoints

--- src/Routes/Collaborator/collaborator.router.ts
+++ src/Routes/Collaborator/collaborator.router.ts
@@ -3,8 +3,8 @@ import { CollaboratorController } from "../../Controller/Collaborator/colaborato
 
 export const routerCollaborator = Router();
 
-routerCollaborator.get("/:role_id", CollaboratorController.getAll);
-routerCollaborator.post("/", CollaboratorController.create);
+routerCollaborator.get("/list/:role_id", CollaboratorController.getAll);
+routerCollaborator.post("/create", CollaboratorController.create);
 //routerCollaborator.get("/:id", CollaboratorController.getCollaborator);
 routerCollaborator.put("/:id", CollaboratorController.update);
 routerCollaborator.delete("/:id", CollaboratorController.delete);
